77bf70aac55960874832149728f1ced57c17509d
Ervin-me <ervin398@gmail.com>
2023-09-12 13:31:18 +0200

    added

---
+++ Ervin_Torok_Diagnos.py
@@ -0,0 +1,26 @@
+import random
+
+def IsPrime(num):
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def Calculate():
+    n = random.randint(1, 99)
+
+    print(f"Prime numbers between 2 and {n}:")
+
+    for currentNum in range(2, n + 1):
+        if IsPrime(currentNum):
+            print(currentNum)
+
+Calculate()
